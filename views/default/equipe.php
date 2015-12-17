ebfe2ca39b9c8373436ac09484ace89554078ec8
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-12-17 11:56:46 +0400

    add https or not on tiles layer url

--- views/default/equipe.php
+++ views/default/equipe.php
@@ -374,7 +374,7 @@ function initAll(){
 									"zoom" : 4,
 									"worldCopyJump" : true });
 
-		L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+		L.tileLayer('//{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
 			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 			subdomains: 'abcd',
 			minZoom: 0,
